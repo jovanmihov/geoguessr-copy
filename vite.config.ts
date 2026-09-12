3412bce0fd50b6e9126564dc4ab3e5cc7cd78e17
Jovan Mihov <jovan.mihov@students.finki.ukim.mk>
2026-09-12 15:24:25 +0200

    commit so it works on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/geoguessr-copy/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
